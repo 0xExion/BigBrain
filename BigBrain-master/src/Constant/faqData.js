a72e23fbef4787fcdac01f436ea25e13d1783f1e
Pomskey <91586560+0xExion@users.noreply.github.com>
2021-09-29 01:38:17 -0500

    updated faq
    faq

--- BigBrain-master/src/Constant/faqData.js
+++ BigBrain-master/src/Constant/faqData.js
@@ -4,23 +4,23 @@ export const faqData = [
     text: "TBA",
   },
   {
-    question: "How many BigBrains can I mint?",
+    question: "How many Big Brains can I mint?",
     text: "Up to 10 per transaction and no limit per wallet",
   },
   {
-    question: "How do I get a BigBrains?",
+    question: "How do I get a Big Brains?",
     text: "Well you can start by hitting the books... Big Brains will be available to mint at BigBrainNFT.io Launch dates will be announced in our Twitter and Discord",
   },
   {
-    question: "What are BigBrains?",
+    question: "What are Big Brains?",
     text: "Big Brain NFTs are 7,777 unique Big Brains on the Ethereum Blockchain. Big Brain is a community-oriented circle of metaverse elites who happen to have themselves a big ol' brain. Can't help it",
   },
   {
-    question: "What is the future of BigBrains?",
+    question: "What is the future of Big Brains?",
     text: "Our aim is to provide opportunities for holders, players, and creators alike to utilize their big brains in a spirit of collaboration. Hope you're ready to use your big brain...",
   },
   {
-    question: "Where does BigBrains donate to?",
+    question: "Where does Big Brains donate to?",
     text: "Big Brain NFT will donate 5 ETH to a mental health organization selected by our community members.",
   },
   {
@@ -28,7 +28,7 @@ export const faqData = [
     text: "Currently we support Metamask for mobile and will be adding more support leading up to launch.",
   },
   {
-    question: "What is the smart contract address of BigBrains?",
+    question: "What is the smart contract address of Big Brains?",
     text: "TBA",
   },
   {
