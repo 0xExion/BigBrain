0c00b7614345ab8a45806663e6b802c83394c8d5
Pomskey <91586560+0xExion@users.noreply.github.com>
2021-10-12 19:21:39 -0500

    update price
    price update from .0777 to .05

--- BigBrain-master/src/pages/Home/Sections/PublicLaunch/PublicLaunch.js
+++ BigBrain-master/src/pages/Home/Sections/PublicLaunch/PublicLaunch.js
@@ -5,7 +5,7 @@ const PublicLaunch = () => {
       <div className={classes.container}>
         <h2>PRESALE MINT - 18TH OF OCTOBER. LAUNCH MINT - 19TH OF OCTOBER.</h2>
         <h2>JOIN OUR DISCORD TO STAY Informed.</h2>
-        <h2>MINT PRICE: 0.07777 ETH</h2>
+        <h2>MINT PRICE: 0.05 ETH</h2>
       </div>
     </div>
   );
