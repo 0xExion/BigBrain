c83efbb42ef3686c8bb963e39e61d3098ef82588
filipdasilva <78722537+filipdasilva@users.noreply.github.com>
2021-10-20 12:12:19 -0700

    Update PublicLaunch.js

--- BigBrain-master/src/pages/Home/Sections/PublicLaunch/PublicLaunch.js
+++ BigBrain-master/src/pages/Home/Sections/PublicLaunch/PublicLaunch.js
@@ -3,8 +3,9 @@ const PublicLaunch = () => {
   return (
     <div>
       <div className={classes.container}>
-        <h2>PRESALE MINT - 8PM CENTRAL 18TH OF OCTOBER. LAUNCH MINT - 8PM CENTRAL 19TH OF OCTOBER.</h2>
-        <h2>JOIN OUR DISCORD TO STAY Informed.</h2>
+        <h2>PRESALE MINT - 8PM CENTRAL 20TH OF OCTOBER.</h2>
+        <h2>LAUNCH MINT - 8PM CENTRAL 21ST OF OCTOBER.</h2>
+        <h2>JOIN OUR DISCORD TO STAY NFORMED.</h2>
         <h2>MINT PRICE: 0.05 ETH</h2>
       </div>
     </div>
